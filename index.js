91f56c3b5942f9e753a322755fb0dbc40acd0221
ingalls <nicholas.ingalls@gmail.com>
2014-03-15 00:10:03 -0500

    Cache Command

---
+++ index.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+var download = require('openaddresses-download'),
+    argv = require('minimist')(process.argv.slice(2)),
+    _ = require('underscore'),
+    fs = require('fs'),
+    ProgressBar = require('progress');
+
+var sourceDir = argv._[0],
+    cacheDir = argv._[1],
+    connectors = download.connectors;
+
+if (!sourceDir || !cacheDir) {
+    throw new Error('usage: openaddress-cache path-to-sources path-to-cache');
+}
+
+var sources = fs.readdirSync(sourceDir);
+
+//Only Keep json
+for (var i = 0; i < sources.length; i++){
+  if (sources[i].indexOf('.json') == -1){
+    sources.splice(i, 1);
+    i--;
+  }
+}
+
+_.each(sources, function(source){
+  var parsed = JSON.parse(fs.readFileSync(sourceDir + source, 'utf8'));
+
+  if (!parsed.data) {
+      throw new Error('no data included in source');
+  }
+
+  var type = connectors.byAddress(parsed.data);
+
+  if (!type) {
+      throw new Error('no connector found');
+  }
+
+  connectors[type](parsed, function(err, stream) {
+      if (!argv.silent) showProgress(stream, type);
+      stream.pipe(fs.createWriteStream(cacheDir + source.replace(".json","")));
+  });
+});
+
+function showProgress(stream, type) {
+    var bar;
+    if (type == 'http') {
+        stream.on('response', function(res) {
+            var len = parseInt(res.headers['content-length'], 10);
+            bar = new ProgressBar('  downloading [:bar] :percent :etas', {
+                complete: '=',
+                incomplete: ' ',
+                width: 20,
+                total: len
+            });
+        });
+    } else if (type == 'ftp') {
+        stream.on('size', function(len) {
+            bar = new ProgressBar('  downloading [:bar] :percent :etas', {
+                complete: '=',
+                incomplete: ' ',
+                width: 20,
+                total: len
+            });
+        });
+    }
+    stream.on('data', function(chunk) {
+        if (bar) bar.tick(chunk.length);
+    }).on('end', function() {
+        if (bar) console.log('\n');
+    });
+}
