3e73d0d7cda572e8c641e24ea969d3750da87f12
ingalls <nicholas.ingalls@gmail.com>
2014-03-19 09:13:50 -0300

    JSHint and Spaces Correction

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ var sources = fs.readdirSync(sourceDir);
 
 //Only retain *.json
 for (var i = 0; i < sources.length; i++) {
-  if (sources[i].indexOf('.json') == -1) {
-    sources.splice(i, 1);
-    i--;
-  }
+    if (sources[i].indexOf('.json') == -1) {
+        sources.splice(i, 1);
+        i--;
+    }
 }
 
 //Begin Downloading Sources
@@ -42,44 +42,44 @@ downloadSource(sourceIndex);
 //Download Each Source
 function downloadSource(index) {
   
-  if (index >= sources.length) {
-    console.log("Complete!");
-    process.exit();
-  }
-  
-  var source = sources[index];
-  
-  this.sourceName = sources[index];
-  this.source = sourceDir + source;
-  
-  parsed = JSON.parse(fs.readFileSync(sourceDir + source, 'utf8'));
-
-  if (!parsed.data | parsed.skip == true) {
-      console.log("Skipping: " + this.source);
-      downloadSource(++sourceIndex);
-  } else if (parsed.data.search(/\/MapServer\/\d+/) !== -1 | parsed.data.search(/\/FeatureServer\/\d+$/) !== -1) {
-      //TODO Remove once ESRI is supported
-      console.log("Skipping ESRI Source: " + this.source);
-      downloadSource(++sourceIndex);
-  } else {
-    console.log("Downloading: " + this.source);
-
-    var type = connectors.byAddress(parsed.data);
-
-    if (!type) {
-        throw new Error('no connector found');
+    if (index >= sources.length) {
+        console.log("Complete!");
+        process.exit();
     }
+    
+    var source = sources[index];
+    
+    this.sourceName = sources[index];
+    this.source = sourceDir + source;
+    
+    parsed = JSON.parse(fs.readFileSync(sourceDir + source, 'utf8'));
 
-    if (parsed.compression != undefined)
-      output = cacheDir + source.replace(".json","") + "." + parsed.compression;
-    else 
-      output = cacheDir + source.replace(".json","");
+    if (!parsed.data || parsed.skip === true) {
+        console.log("Skipping: " + this.source);
+        downloadSource(++sourceIndex);
+    } else if (parsed.data.search(/\/MapServer\/\d+/) !== -1 || parsed.data.search(/\/FeatureServer\/\d+$/) !== -1) {
+        //TODO Remove once ESRI is supported
+        console.log("Skipping ESRI Source: " + this.source);
+        downloadSource(++sourceIndex);
+    } else {
+      console.log("Downloading: " + this.source);
 
-    connectors[type](parsed, function(err, stream) {
-        if (!argv.silent) showProgress(stream, type);
-        stream.pipe(fs.createWriteStream(output));
-    });
-  }
+      var type = connectors.byAddress(parsed.data);
+
+      if (!type) {
+          throw new Error('no connector found');
+      }
+
+      if (parsed.compression !== undefined)
+          output = cacheDir + source.replace(".json","") + "." + parsed.compression;
+      else 
+          output = cacheDir + source.replace(".json","");
+
+      connectors[type](parsed, function(err, stream) {
+          if (!argv.silent) showProgress(stream, type);
+          stream.pipe(fs.createWriteStream(output));
+      });
+    }
 }
 
 
@@ -115,56 +115,55 @@ function showProgress(stream, type) {
 
 function checkHash(output) {
   
-  var fd = fs.createReadStream(output);
-  var hash = crypto.createHash('md5');
-  hash.setEncoding('hex');
+    var fd = fs.createReadStream(output);
+    var hash = crypto.createHash('md5');
+    hash.setEncoding('hex');
 
-  fd.on('end', function() {
-    hash.end();
-    var md5Hash = hash.read();
-    
-    if (parsed.fingerprint != md5Hash)
-      updateCache(md5Hash);
-    else {
-      fs.unlinkSync(output);
-      downloadSource(++sourceIndex);
-    }
-  });
+    fd.on('end', function() {
+        hash.end();
+        var md5Hash = hash.read();
+        
+        if (parsed.fingerprint != md5Hash)
+          updateCache(md5Hash);
+        else {
+            fs.unlinkSync(output);
+            downloadSource(++sourceIndex);
+        }
+    });
 
-  fd.pipe(hash);
+    fd.pipe(hash);
 }
 
 function updateManifest() {
-  fs.writeFileSync(this.source, JSON.stringify(parsed, null, 4));
-  console.log("  Updating Manifest of " + this.source);
+    fs.writeFileSync(this.source, JSON.stringify(parsed, null, 4));
+    console.log("  Updating Manifest of " + this.source);
 }
 
 function updateCache(md5Hash) {
-  parsed.fingerprint = md5Hash;
-  parsed.version = time().format('YYYYMMDD');
-  parsed.cache = "http://s3.amazonaws.com/openaddresses/" + this.sourceName.replace(".json", ".zip");
-  
-  console.log("  Updating s3 with " + this.source);
-  
-  var s3 = new AWS.S3();
-  fs.readFile(output, function (err, data) {
-    if (err)
-      throw new Error('Could not find data to upload'); 
+    parsed.fingerprint = md5Hash;
+    parsed.version = time().format('YYYYMMDD');
+    parsed.cache = "http://s3.amazonaws.com/openaddresses/" + this.sourceName.replace(".json", ".zip");
     
-    var buffer = new Buffer(data, 'binary');
-
+    console.log("  Updating s3 with " + this.source);
+    
     var s3 = new AWS.S3();
-    
-    s3.client.putObject({
-      Bucket: 'openaddresses',
-      Key: this.sourceName.replace(".json", ".zip"),
-      Body: buffer,
-      ACL: 'public-read'
-    }, function (response) {
-      console.log('  Successfully uploaded package.');
-      updateManifest();
-      downloadSource(++sourceIndex);
-    });
-  });
+    fs.readFile(output, function (err, data) {
+        if (err)
+            throw new Error('Could not find data to upload'); 
+        
+        var buffer = new Buffer(data, 'binary');
 
+        var s3 = new AWS.S3();
+        
+        s3.client.putObject({
+            Bucket: 'openaddresses',
+            Key: this.sourceName.replace(".json", ".zip"),
+            Body: buffer,
+            ACL: 'public-read'
+        }, function (response) {
+            console.log('  Successfully uploaded package.');
+            updateManifest();
+            downloadSource(++sourceIndex);
+        });
+    });
 }
